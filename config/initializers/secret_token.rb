6b14563883741ade64519a37b4450a822f12f256
Chris Kimpton <chris@kimptoc.net>
2011-11-16 06:29:06 +0000

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsFunnies::Application.config.secret_token = 'bf8c2827fdb4bed8c6a25eab64a1e389863ac1923a6e63cd96c39d4e4e0fbb2a314a08550cf0cfea1af57459d0d55deb7743e8362306b70bb5dd10cb39c7fa7b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CsFunnies::Application.config.session_store :cookie_store, key: '_cs-funnies_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CsFunnies::Application.config.session_store :active_record_store
